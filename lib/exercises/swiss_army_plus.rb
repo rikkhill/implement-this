2aeed97f48be5a42d2819514f7766164bc3a3033
Rikk Hill <rikk@unashamedly.co.uk>
2017-11-10 15:45:34 +0000

    Added subtraction and division on Swiss Integer

--- lib/exercises/swiss_army_plus.rb
+++ lib/exercises/swiss_army_plus.rb
@@ -3,16 +3,32 @@
 # subtract and divide operators for integers.
 # Use only the add operator."
 
+# I like the cut of this problem's jib
+
+# This seems to ban most common binary operators, but doesn't
+# say anything about the unary `-` operator. This problem seems
+# extraordinarily easy if given unary `-` but extraordinarily hard
+# if not given it. As such, I'm going to judiciously choose when
+# unary `-` is warranted
+
+
+# The SwissInteger class is a first-pass, quick-and-dirty attempt
+
 class SwissInteger
 
   def initialize x
     @x = x
   end
 
+  # An obvious way of implementing the plus operator is simple transparent usage
   def +(y)
     @x + y
   end
 
+
+  # One way of implementing multiplication for positive integers is to just iterate
+  # over the range. We could also do this by taking the cardinality of the cartesian
+  # product
   def *(y)
     z = 0
 
@@ -22,5 +38,63 @@ class SwissInteger
     z
   end
 
+
+  def -(y)
+
+    # We have three cases: x > y,  y > x, and x == y
+
+    overage = 0
+    # In the first case we'll get a positive integer
+    if @x > y
+      for i in 1..@x
+        if i > y
+          overage += 1
+        end
+      end
+
+      overage
+
+    # In the second case, we'll get a negative number
+    elsif y > @x
+      for i in 1..y
+        if i > @x
+          overage += 1
+        end
+      end
+
+      # I am fairly sure it is impossible to produce a negative
+      # integer from positive integers using only the `+` operator
+      # (since the positive integers are closed under addition),
+      # therefore I feel fine in using unary `-` in this case
+      -(overage)
+
+    # In the final case, the difference must be zero
+    else
+      0
+    end
+  end
+
+  # Division
+  def /(y)
+
+    divisor = 0
+    counter = 0
+
+    for i in 1..@x
+      divisor += 1
+
+      if divisor == y
+        divisor = 0
+        counter += 1
+      end
+
+    end
+
+    counter
+
+  end
+
 end
+
+
 
--- test/exercises.rb
+++ test/exercises.rb
@@ -1,19 +1,35 @@
 require 'test/unit'
-require_relative "../lib/exercises/swiss_army_plus"
+require_relative '../lib/exercises/swiss_army_plus'
 
-class TestSimple < Test::Unit::TestCase
+# These are not massively principled.
+# They should probably test group axioms or something
+
+class TestArithmetic < Test::Unit::TestCase
   def test_plus
-    assert_equal((SwissInteger.new 10) + 5, 15, "10 + 5 = 15")
-    assert_equal((SwissInteger.new 1) + 1, 2, "1 + 1 = 2")
-    assert_equal((SwissInteger.new 2) + 2, 4, "2 + 2 = 4")
-    assert_equal((SwissInteger.new 100) + 100, 200, "100 + 100 = 200")
+    assert_equal((SwissInteger.new 10) + 5, 15, '10 + 5 = 15')
+    assert_equal((SwissInteger.new 1) + 1, 2, '1 + 1 = 2')
+    assert_equal((SwissInteger.new 2) + 2, 4, '2 + 2 = 4')
+    assert_equal((SwissInteger.new 100) + 100, 200, '100 + 100 = 200')
   end
 
   def test_multiply
-    assert_equal((SwissInteger.new 10) * 5, 50, "10 * 5 = 50")
-    assert_equal((SwissInteger.new 1) * 1, 1, "1 * 1 = 1")
-    assert_equal((SwissInteger.new 2) * 2, 4, "2 * 2 = 4")
-    assert_equal((SwissInteger.new 100) * 100, 10000, "100 * 100 = 10000")
+    assert_equal((SwissInteger.new 10) * 5, 50, '10 * 5 = 50')
+    assert_equal((SwissInteger.new 1) * 1, 1, '1 * 1 = 1')
+    assert_equal((SwissInteger.new 2) * 2, 4, '2 * 2 = 4')
+    assert_equal((SwissInteger.new 100) * 100, 10000, '100 * 100 = 10000')
   end
 
+  def test_minus
+    assert_equal((SwissInteger.new 10) - 5, 5, '10 - 5 = 5')
+    assert_equal((SwissInteger.new 1) - 1, 0, '1 - 1 = 0')
+    assert_equal((SwissInteger.new 2) - 4, -2, '2 - 4 = -2')
+    assert_equal((SwissInteger.new 100) - 10, 90, '100 - 10 = 90')
+  end
+
+  def test_divide
+    assert_equal((SwissInteger.new 10) / 2, 5, '10 / 2 = 5')
+    assert_equal((SwissInteger.new 13) / 5, 2, '13 / 5 = 2')
+    assert_equal((SwissInteger.new 2) / 2, 1, '2 / 2 = 1')
+    assert_equal((SwissInteger.new 100) / 10, 10, '100 / 10 = 10')
+  end
 end
